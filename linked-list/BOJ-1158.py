e20d7543518eaaf291c79e8ead0b8851d8545419
atg0831 <atg0831@naver.com>
2021-04-02 03:11:59 +0900

    Refactor: modify code

--- linked-list/BOJ-1158.py
+++ linked-list/BOJ-1158.py
@@ -1,10 +1,10 @@
-N,K=map(int,input().split())
+N, K = map(int, input().split())
 
-seq=[i for i in range(1,N+1)]
-idx=0
-answer=[]
+seq = [i for i in range(1, N+1)]
+idx = 0
+answer = []
 while seq:
-    idx=(idx+K-1)%len(seq)
+    idx = (idx+K-1) % len(seq)
     answer.append(str(seq.pop(idx)))
-    
-print("<"+", ".join(answer)+">")
+
+print('<' + ', '.join(answer) + '>')
