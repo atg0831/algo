969901ba5d869dfcd0b5434d21b86a2ecfdc282e
atg0831 <atg0831@naver.com>
2021-04-16 00:38:54 +0900

    solved: dfs/bfs & unsolved problems

--- dfs&bfs/BOJ-2667.py
+++ dfs&bfs/BOJ-2667.py
@@ -1,36 +1,40 @@
 import sys
-N=int(input())
+N = int(input())
 
 houses = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(N)]
 
-dir = [(-1,0),(1,0),(0,1),(0,-1)]
-def posiible_path(x,y):
-    if (0<=x<N) and (0<=y<N):
+dir = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+
+
+def posiible_path(x, y):
+    if (0 <= x < N) and (0 <= y < N):
         return True
 
     return False
 
-def dfs(x,y,selected):
-    visited[x][y]=True
-    selected.append((x,y))
+
+def dfs(x, y, selected):
+    visited[x][y] = True
+    selected.append((x, y))
 
     for dirx, diry in dir:
         nextx = x+dirx
         nexty = y+diry
 
-        if posiible_path(nextx,nexty):
-            if not visited[nextx][nexty] and houses[nextx][nexty]==1:
-                dfs(nextx,nexty,selected)
+        if posiible_path(nextx, nexty):
+            if not visited[nextx][nexty] and houses[nextx][nexty] == 1:
+                dfs(nextx, nexty, selected)
 
-num_of_houses=0
-kind_of_houses=[]
-visited=[[False for _ in range(N)]for _ in range(N)]
+
+num_of_houses = 0
+kind_of_houses = []
+visited = [[False for _ in range(N)]for _ in range(N)]
 for i in range(N):
     for j in range(N):
-        if not visited[i][j] and houses[i][j]==1:
-            selected=[]
-            dfs(i,j,selected)
-            num_of_houses+=1
+        if not visited[i][j] and houses[i][j] == 1:
+            selected = []
+            dfs(i, j, selected)
+            num_of_houses += 1
             kind_of_houses.append(len(selected))
 
 print(num_of_houses)
