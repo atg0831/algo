0ee53be5a13b08a5811c72869395b362086dc6c2
atg0831 <atg0831@naver.com>
2021-05-06 18:21:42 +0900

    solved: solved dp problem

--- dp/BOJ-11052.py
+++ dp/BOJ-11052.py
@@ -1,6 +1,5 @@
 N = int(input())
 pay = [0] + list(map(int, input().split()))
-# dp = [i for i in range(1, N+1)]
 dp = [0] * (N+1)
 dp[1] = pay[1]
 
@@ -10,3 +9,4 @@ for i in range(2, N+1):
         dp[i] = max(dp[i], dp[i-j] + pay[j])
 
 print(dp[N])
+
--- dp/BOJ-2240.py
+++
@@ -1,7 +0,0 @@
-T, W = map(int, input().split())
-loc = [0]
-dp = [0] * (T+1)
-
-for _ in range(T):
-    loc.append(int(input()))
-    
